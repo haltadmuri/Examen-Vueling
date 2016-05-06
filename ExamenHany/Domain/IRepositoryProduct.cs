08b7457294838e1dea5967515dcd7032a4b3fb8e
haltadmuri <haltadmuri@gmail.com>
2016-05-06 11:40:14 +0200

    Ultimo

--- ExamenHany/Domain/IRepositoryProduct.cs
+++ ExamenHany/Domain/IRepositoryProduct.cs
@@ -10,7 +10,6 @@ namespace Domain
     {
         Product Get(int id);
         IEnumerable<Product> GetAll();
-        
         Product Add(Product product);
         void Update(Product product);
         void Delete(Product product);
