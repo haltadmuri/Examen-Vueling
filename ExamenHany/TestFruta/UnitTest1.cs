af22336430e3ad9fa370f2234aea4c093f878b1b
haltadmuri <haltadmuri@gmail.com>
2016-05-06 13:17:41 +0200

    Test Y wcf

---
+++ ExamenHany/TestFruta/UnitTest1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NMock;
+using Domain;
+using FruitStore.Controllers;
+using System.Collections.Generic;
+using Infraestructure;
+
+namespace TestFruta
+{
+    [TestClass]
+    public class UnitTest1
+    {
+         private MockFactory _factory = new MockFactory();
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            _factory.VerifyAllExpectationsHaveBeenMet();
+            _factory.ClearExpectations();
+        }
+
+
+        [TestMethod]
+        public void NumberOfProductsEqualsOne()
+        {
+            // Arrange
+            var repository = _factory.CreateMock<IRepositoryProduct>();
+            var unitOfWork = _factory.CreateMock<IUnitOfWork>();
+            var productController = new ProductController(repository.MockObject, unitOfWork.MockObject);
+            
+            var products = new HashSet<Product>() {
+                new Product() {Name = "pera"}
+            };
+            repository.Expects.One.Method(c => c.GetAll()).WillReturn(products);
+            repository.Expects.One.MethodWith(c => c.Get(1)).WillReturn(new Product() { Name ="Antonio"});
+            unitOfWork.Expects.One.Method(c => c.Dispose());
+
+            // Act
+            //var result = ((productController.Index() as ViewResult).Model) as List<Product>;
+            //productController.Edit(1);
+            //productController.Dispose();
+            // Assert
+            //Assert.AreEqual(1, result.Count);
+            //Assert.AreEqual("pera", result[0].Name);
+        }
+    }
+}
---
+++ ExamenHany/WcfServiceFruit/IServiceFruit.cs
@@ -0,0 +1,42 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfServiceFruit
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IServiceFruit" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IServiceFruit
+    {
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json, UriTemplate = "/GetArticleList")]
+        IEnumerable<Product> GetArticleList();
+
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json, UriTemplate = "/GetArticle/{id}")]
+        Product GetArticle(string id);
+
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json, UriTemplate = "/Article/Add/{name}/{price}")]
+        Product Add(string name, string price);
+
+
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json, UriTemplate = "/GetDetalleVentaList")]
+        IEnumerable<Invoice> GetDetalleVentaList();
+
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json, UriTemplate = "/AddDetalleVentaList/{IdArticles}/{Quantity}/{IdSale}")]
+        Invoice AddDetalleVentaList(string IdProduct, string Quantity, string IdTicket);
+
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json, UriTemplate = "/AddVentaList/{Date}/{Total}")]
+        Ticket AddVentaList(string Date, string Total);
+     
+    }
+}
